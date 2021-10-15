9d76493f400d0e7dbea43815a1e6fc698e91b672
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-15 15:51:19 +0530

    added search

--- src/pages/TodosList.tsx
+++ src/pages/TodosList.tsx
@@ -1,66 +1,89 @@
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../redux/store";
 import { deleteTodo, completeTodo } from "../redux/todoSlice";
-import SearchBar from "../components/SearchBar";
+// import SearchBar from "../components/SearchBar";
+import TextField from "@mui/material/TextField";
 
 export default function TodosList() {
   const dispatch = useDispatch();
   const todos = useSelector((state: RootState) => state.todos);
 
+  const [todoList, setTodoList] = useState(todos);
+
   let size = 2;
   let arrayOfArrays = [];
 
-  for (let i = 0; i < todos.length; i += size) {
-    arrayOfArrays.push(todos.slice(i, i + size));
+  for (let i = 0; i < todoList.length; i += size) {
+    arrayOfArrays.push(todoList.slice(i, i + size));
   }
+
+  function handleChange(searchedVal: string) {
+    if (searchedVal === "" || searchedVal === null) {
+      setTodoList(todos);
+    } else {
+      const filteredTodos = todoList.filter((row) =>
+        row.text.toLowerCase().includes(searchedVal.toLowerCase())
+      );
+      setTodoList(filteredTodos);
+    }
+  }
+
+  useEffect(() => {
+    setTodoList(todos);
+  }, [todos]);
 
   return (
     <>
       <section className="d-flex flex-column align-items-center">
-        <div className="mb-5 mb-md-4">
-          <SearchBar />
+        <div className="mb-4">
+          <TextField onChange={(e) => handleChange(e.target.value)} />
         </div>
         <div
           className="container-fluid scroll-bar"
           style={{ maxHeight: "85vh", overflowY: "auto" }}
         >
-          {arrayOfArrays.map((chunk, index) => (
-            <div className="row" key={index}>
-              {chunk.map((todo) => (
-                <div className="col-md-6" key={todo.id}>
-                  <div className="card mb-4">
-                    <div className="card-body">
-                      {todo.completed ? (
-                        <h5 className="text-decoration-line-through card-title">
-                          {todo.text}
-                        </h5>
-                      ) : (
-                        <h5 className="card-title">{todo.text}</h5>
-                      )}
-                      <div className="d-flex justify-content-between">
-                        <button
-                          className="btn btn-primary mt-2"
-                          onClick={() => {
-                            dispatch(completeTodo(todo.id));
-                          }}
-                        >
-                          Done
-                        </button>
-                        <button
-                          className="btn btn-secondary mt-2"
-                          onClick={() => {
-                            dispatch(deleteTodo(todo.id));
-                          }}
-                        >
-                          Delete
-                        </button>
+          {arrayOfArrays.length > 0 ? (
+            arrayOfArrays.map((chunk, index) => (
+              <div className="row" key={index}>
+                {chunk.map((todo) => (
+                  <div className="col-md-6" key={todo.id}>
+                    <div className="card mb-4">
+                      <div className="card-body">
+                        {todo.completed ? (
+                          <h5 className="text-decoration-line-through card-title">
+                            {todo.text}
+                          </h5>
+                        ) : (
+                          <h5 className="card-title">{todo.text}</h5>
+                        )}
+                        <div className="d-flex justify-content-between">
+                          <button
+                            className="btn btn-primary mt-2"
+                            onClick={() => {
+                              dispatch(completeTodo(todo.id));
+                            }}
+                          >
+                            Done
+                          </button>
+                          <button
+                            className="btn btn-secondary mt-2"
+                            onClick={() => {
+                              dispatch(deleteTodo(todo.id));
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </div>
-          ))}
+                ))}
+              </div>
+            ))
+          ) : (
+            <p className="text-center">No results found</p>
+          )}
         </div>
       </section>
     </>
