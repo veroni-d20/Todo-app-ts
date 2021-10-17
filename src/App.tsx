6fd8034d9d32cee89402ea7314b2a6cb48eae3f4
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-17 23:34:34 +0530

    Changed file structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import {
   Redirect,
 } from "react-router-dom";
 
-import LandingPage from "./pages/LandingPage";
+import Login from "./pages/LoginPage";
+import Home from "./pages/Home";
 import Error404 from "./pages/Error404";
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/home" component={Home} />
         <Route exact path="/404" component={Error404} />
         <Redirect to="/404" />
       </Switch>
