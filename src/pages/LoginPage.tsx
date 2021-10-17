903ea0c75c3d8ac03ad9409707cf9373c9eb48f8
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-18 00:29:39 +0530

    Login UI

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,26 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section className="full-vh d-flex align-justify-center">
+        <div className="text-center shadow p-5 bg-body rounded">
+          <TextField
+            className="d-block mb-5"
+            label="Email"
+            variant="filled"
+            type="email"
+          />
+          <TextField
+            className="d-block mb-5"
+            label="Password"
+            variant="filled"
+            type="password"
+          />
+          <Button variant="contained">Login</Button>
+        </div>
+      </section>
+    </>
   );
 }
