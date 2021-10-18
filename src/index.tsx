07f6d3f8d187711065f5741b4bce015bc192668b
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-18 23:29:28 +0530

    Added Auth provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/scss/style.scss";
 import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <AuthProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
