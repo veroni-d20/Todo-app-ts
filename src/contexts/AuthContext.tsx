2340be6b5202a40ac45e9beb8bce98400a8ae9cc
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-18 14:20:54 +0530

    created AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,35 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+import { auth } from "../firebase";
+
+const AuthContext = createContext<any>(null);
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export default function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<object | null>(null);
+
+  function login({ email, password }: { email: string; password: string }) {
+    auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = { user, login };
+
+  return (
+    <AuthContext.Provider value={value}> {children} </AuthContext.Provider>
+  );
+}
