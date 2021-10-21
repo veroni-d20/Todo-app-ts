8ffed29d8929cdd4913b8094782ab1c0c6f7520f
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-21 09:48:12 +0530

    UI changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,9 @@ import TodosList from "./TodosList";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../redux/slices/todoSlice";
 import { useAuth } from "../contexts/AuthContext";
+import Fab from "@mui/material/Fab";
+import Tooltip from "@mui/material/Tooltip";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export default function LandingPage() {
   const { logout } = useAuth();
@@ -20,7 +23,7 @@ export default function LandingPage() {
   return (
     <>
       <main className="full-vh d-md-flex justify-content-between align-items-center mx-4">
-        <section className="d-md-flex flex-md-column align-justify-center w-md-50">
+        <section className="d-md-flex flex-column align-justify-center w-md-50 vh-md-100 addtodo-color">
           <div className="text-center text-md-start">
             <h1 className="mt-3 mt-md-0">To Do App</h1>
             <div className="input-group mt-3">
@@ -40,19 +43,17 @@ export default function LandingPage() {
                 Add ToDo
               </button>
             </div>
-            <div className="mt-4 mt-md-5">
-              <button
-                className="btn btn-outline-black"
-                type="button"
-                onClick={logout}
-              >
-                Logout
-              </button>
-            </div>
           </div>
         </section>
         <section className="w-100 mt-2 mt-md-0">
           <TodosList />
+        </section>
+        <section className="fab-button">
+          <Tooltip title="Logout">
+            <Fab color="primary" aria-label="logout" onClick={logout}>
+              <LogoutIcon />
+            </Fab>
+          </Tooltip>
         </section>
       </main>
     </>
