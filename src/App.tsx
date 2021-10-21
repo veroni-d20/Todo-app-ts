0189f8c30d043d10f7933179fb43f602e85a8b7e
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-21 07:20:52 +0530

    added HomeRoute

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import Login from "./pages/LoginPage";
 import Home from "./pages/Home";
 import Error404 from "./pages/Error404";
 
+import HomeRoute from "./routes/HomeRoute";
+
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
+        <HomeRoute exact path="/home" component={Home} />
         <Route exact path="/404" component={Error404} />
         <Redirect to="/404" />
       </Switch>
