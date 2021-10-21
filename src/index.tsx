68211e573166a184846240e6a3568a83b82fe76a
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-21 11:51:17 +0530

    added mui themes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/scss/style.scss";
+import { theme } from "./components/MUIThemes";
+import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Provider } from "react-redux";
@@ -10,7 +12,9 @@ ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
       <Provider store={store}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </Provider>
     </AuthProvider>
   </React.StrictMode>,
