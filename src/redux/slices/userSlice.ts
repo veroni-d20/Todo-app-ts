6d959cd90cb94bc98f940f592c220d9bf3971876
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-22 08:53:48 +0530

    added userState

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -6,13 +6,21 @@ interface IUser {
   lastName: string;
 }
 
-const initialState: IUser[] = [
-  {
-    id: 1,
-    firstName: "Veroni",
-    lastName: "Shwetha",
-  },
-];
+interface IUserPayload {
+  userState: boolean;
+  users: IUser[];
+}
+
+const initialState: IUserPayload = {
+  userState: false,
+  users: [
+    {
+      id: 1,
+      firstName: "Veroni",
+      lastName: "Shwetha",
+    },
+  ],
+};
 
 const userSlice = createSlice({
   name: "userSlice",
@@ -20,7 +28,7 @@ const userSlice = createSlice({
   reducers: {
     getUser: () => {},
     setUser: (state, action: PayloadAction<IUser>) => {
-      state.push(action.payload);
+      state.users.push(action.payload);
     },
   },
 });
