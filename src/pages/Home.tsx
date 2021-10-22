fb47338b83f554e7fcdd83e306350e351aefcd68
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-22 08:19:09 +0530

    changed import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,14 @@
+import { Fab, Tooltip } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SnackBar from "../components/SnackBar";
+import Loader from "../components/Loader";
+import TodosList from "./TodosList";
 import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import TodosList from "./TodosList";
 import { addTodo } from "../redux/slices/todoSlice";
 import { getUser } from "../redux/slices/userSlice";
 import { RootState } from "../redux/store";
 import { useAuth } from "../contexts/AuthContext";
-import Fab from "@mui/material/Fab";
-import Tooltip from "@mui/material/Tooltip";
-import LogoutIcon from "@mui/icons-material/Logout";
-import SnackBar from "../components/SnackBar";
-import Loader from "../components/Loader";
 
 export default function LandingPage() {
   const [loading, setLoading] = useState(true);
