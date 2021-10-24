03b70b225d4111ca25bd4b3987a77e0b9541d21d
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-24 10:20:44 +0530

    removed userState

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -6,21 +6,13 @@ interface IUser {
   lastName: string;
 }
 
-interface IUserPayload {
-  userState: boolean;
-  users: IUser[];
-}
-
-const initialState: IUserPayload = {
-  userState: false,
-  users: [
-    {
-      id: 1,
-      firstName: "Veroni",
-      lastName: "Shwetha",
-    },
-  ],
-};
+const initialState: IUser[] = [
+  {
+    id: 1,
+    firstName: "Veroni",
+    lastName: "Shwetha",
+  },
+];
 
 const userSlice = createSlice({
   name: "userSlice",
@@ -28,8 +20,7 @@ const userSlice = createSlice({
   reducers: {
     getUser: () => {},
     setUser: (state, action: PayloadAction<IUser>) => {
-      state.users.push(action.payload);
-      state.userState = true;
+      state.push(action.payload);
     },
   },
 });
