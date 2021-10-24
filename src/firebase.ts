0f39c73b719e331ecb9fb689658ac5e31ef9ff9c
Joshuafrankle <joshuafrankle7@gmail.com>
2021-10-24 10:25:23 +0530

    removed

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 require("dotenv").config();
 
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyBA1D0hthlfNgQGTaLI9M2mcZdqVLloeNg",
-  authDomain: "todoapp-9d3a2.firebaseapp.com",
-  projectId: "todoapp-9d3a2",
-  storageBucket: "todoapp-9d3a2.appspot.com",
-  messagingSenderId: "481544986224",
-  appId: "1:481544986224:web:e8b1e62fbaa7e1c9135eb5",
-});
+const app = firebase.initializeApp({});
 
 console.log(app);
 
